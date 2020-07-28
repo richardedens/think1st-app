ed583f513ead7e97dc0caec870b31147b9bbbe76
Gerhard Richard Edens <richardedens@outlook.com>
2020-07-28 21:05:33 +0200

    Fixing allot of things.

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,40 @@
 /**
  * Tea - Version 1.0.0.
  */
-tea(document).ready(function () {
+tea(document).ready(function() {
+
+    /**
+     * Get inscribed area size
+     *
+     * @param int oW outer width
+     * @param int oH outer height
+     * @param int iW inner width
+     * @param int iH inner height
+     * @param bool R resize if smaller
+     */
+    function getInscribedArea(oW, oH, iW, iH, R) {
+        if (!R && iW < oW && iH < oH) {
+            return {
+                "h": iH,
+                "w": iW
+            };
+        }
+        if ((oW / oH) > (iW / iH)) {
+            return {
+                "h": oH,
+                "w": Math.round(oH * iW / iH)
+            }
+        } else {
+            return {
+                "h": Math.round(oW * iH / iW),
+                "w": oW
+            };
+        }
+    }
 
     function renderTypeScript() {
         // Prerequisites.
-        tea("#tea-editor").getModel().then(function (json) {
+        tea("#tea-editor").getModel().then(function(json) {
             console.log(json);
             let className = tea("#file-name").text().replace(".tea", "");
 
@@ -16,7 +45,7 @@ tea(document).ready(function () {
 
             // Compile code
             console.log(json);
-            json.code.reverse().map(function (item, index) {
+            json.code.reverse().map(function(item, index) {
                 if (item['type'] === 'start') {
                     code += "\trun() {\r\n";
                 }
@@ -39,7 +68,7 @@ tea(document).ready(function () {
 
     function renderPHP() {
         // Prerequisites.
-        tea("#tea-editor").getModel().then(function (json) {
+        tea("#tea-editor").getModel().then(function(json) {
             console.log(json);
             let className = tea("#file-name").text().replace(".tea", "");
 
@@ -50,7 +79,7 @@ tea(document).ready(function () {
 
             // Compile code
             console.log(json);
-            json.code.reverse().map(function (item, index) {
+            json.code.reverse().map(function(item, index) {
                 if (item['type'] === 'start') {
                     code += "\tfunction run() {\r\n";
                 }
@@ -72,7 +101,7 @@ tea(document).ready(function () {
 
     function renderPython() {
         // Prerequisites.
-        tea("#tea-editor").getModel().then(function (json) {
+        tea("#tea-editor").getModel().then(function(json) {
             console.log(json);
             let className = tea("#file-name").text().replace(".tea", "");
 
@@ -83,12 +112,11 @@ tea(document).ready(function () {
 
             // Compile code
             console.log(json);
-            json.code.reverse().map(function (item, index) {
+            json.code.reverse().map(function(item, index) {
                 if (item['type'] === 'start') {
                     code += "\tdef run():\r\n";
                 }
-                if (item['type'] === 'stop') {
-                }
+                if (item['type'] === 'stop') {}
                 if (item['type'] === 'action') {
                     code += "\t\taction()\r\n";
                 }
@@ -104,7 +132,11 @@ tea(document).ready(function () {
     tea("#tea-editor").poor();
 
     tea("#add-action").on('click', () => {
-        tea("#tea-editor").addAction();
+        let success = tea("#tea-editor").addAction();
+        if (!success) {
+            tea("#tea-information-msg").html("There needs to be a START in your TEA script.");
+            tea("#tea-information-modal").modal();
+        }
         renderTypeScript();
         renderPython();
         renderPHP();
@@ -130,6 +162,20 @@ tea(document).ready(function () {
         renderTypeScript();
         renderPython();
         renderPHP();
+    });
+
+    tea('.scrollbar-map').scrollbar({
+        "onInit": function() {
+            this.container.find('.scroll-element_outer').appendTo(this.wrapper);
+        },
+        "onUpdate": function(container) {
+            var s = getInscribedArea(140, 140, this.scrollx.size, this.scrolly.size);
+            this.scrolly.scroll.height(s.h);
+            this.scrollx.scroll.width(s.w);
+        },
+        "scrollx": $('.scrollbar-map .scroll-element_outer'),
+        "scrolly": $('.scrollbar-map .scroll-element_outer'),
+        "stepScrolling": false
     });
 
 });
--- serve.js
+++ serve.js
@@ -1,9 +1,43 @@
+/* ------------------------------------------------------------------------
+ *         ║    ║    ║   ║ 
+ *        ║    ║    ║   ║ 
+ *       ║    ║    ║   ║ 
+ *        ║    ║    ║   ║ 
+ * 
+ *      ██████████████████████ 
+ *     ██▒░░░░░░░░░░░░░░░▒██▒▒██ 
+ *     ██▒░░░░░░░░░░░▒░▒░▒██▒▒██ 
+ *     ███▒▒░░░░░░▒░░▒▒▒▒█████ 
+ *       ██▒░░░░▒░▒░▒▒▒▒███ 
+ *        ██▒▒▒▒▒▒▒▒▒▒▒███ 
+ *         █████████████
+ *       ══════════════════
+ *        _______         
+ *       |__   __|        
+ *          | | ___  __ _ 
+ *          | |/ _ \/ _` |
+ *          | |  __/ (_| |
+ *          |_|\___|\__,_|
+ * 
+ *          version 1.0.0
+ * 
+ * Author: Gerhard Richard Edens
+ * 
+ * Description:
+ * 
+ * The development expressjs server.
+ * 
+ */
+
 const express = require('express')
 const app = express()
 const port = 3000
+const open = require('open')
 
-app.use(express.static('demo'))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/views/dashboard.html'))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
+open("http://localhost:3000")
