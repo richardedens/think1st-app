e1ff403669ca2b1492b048fd7c2dfe90b415d69d
Gerhard Richard Edens <richardedens@outlook.com>
2020-08-02 22:03:01 +0200

    Hard work.. but getting there to integrate archimate 3.1 into tea 1.0

--- public/tea/tea.js
+++ public/tea/tea.js
@@ -366,15 +366,21 @@ window.Tea = tea = (function() {
 
     }
 
+    function moveElement(e) {
+        if (!e.target.classList.contains("tea-archimate-innertext")) {
+            if (isFirefox) {
+                e.target.style.left = (fireFoxEvt.pageX - teaEditor.offsetLeft - Math.ceil(e.target.style.width.replace("px", "") / 2)) + teaEditor.scrollLeft + "px";
+                e.target.style.top = (fireFoxEvt.pageY - teaEditor.offsetTop - Math.ceil(e.target.style.height.replace("px", "") / 2)) + teaEditor.scrollTop + "px";
+            } else {
+                e.target.style.left = (e.pageX - teaEditor.offsetLeft - Math.ceil(e.target.style.width.replace("px", "") / 2)) + teaEditor.scrollLeft + "px";
+                e.target.style.top = (e.pageY - teaEditor.offsetTop - Math.ceil(e.target.style.height.replace("px", "") / 2)) + teaEditor.scrollTop + "px";
+            }
+        }
+    }
+
     // Setup dragging elements.
     function handleDragStart(e) {
-        if (isFirefox) {
-            e.target.style.left = (fireFoxEvt.pageX - teaEditor.offsetLeft - (e.target.style.width.replace("px", "") / 2)) + teaEditor.scrollLeft + "px";
-            e.target.style.top = (fireFoxEvt.pageY - teaEditor.offsetTop - (e.target.style.height.replace("px", "") / 2)) + teaEditor.scrollTop + "px";
-        } else {
-            e.target.style.left = (e.pageX - teaEditor.offsetLeft - (e.target.style.width.replace("px", "") / 2)) + teaEditor.scrollLeft + "px";
-            e.target.style.top = (e.pageY - teaEditor.offsetTop - (e.target.style.height.replace("px", "") / 2)) + teaEditor.scrollTop + "px";
-        }
+        moveElement(e);
         // Data Transfer
         img = new Image();
         e.dataTransfer.effectAllowed = 'all';
@@ -383,39 +389,22 @@ window.Tea = tea = (function() {
     }
 
     function handleDrag(e) {
-        if (isFirefox) {
-            e.target.style.left = (fireFoxEvt.pageX - teaEditor.offsetLeft - Math.ceil(e.target.style.width.replace("px", "") / 2)) + teaEditor.scrollLeft + "px";
-            e.target.style.top = (fireFoxEvt.pageY - teaEditor.offsetTop - Math.ceil(e.target.style.height.replace("px", "") / 2)) + teaEditor.scrollTop + "px";
-        } else {
-            e.target.style.left = (e.pageX - teaEditor.offsetLeft - Math.ceil(e.target.style.width.replace("px", "") / 2)) + teaEditor.scrollLeft + "px";
-            e.target.style.top = (e.pageY - teaEditor.offsetTop - Math.ceil(e.target.style.height.replace("px", "") / 2)) + teaEditor.scrollTop + "px";
-        }
+        moveElement(e);
     };
 
     function handleDrop(e) {
-        if (isFirefox) {
-            e.target.style.left = (fireFoxEvt.pageX - teaEditor.offsetLeft - Math.ceil(e.target.style.width.replace("px", "") / 2)) + teaEditor.scrollLeft + "px";
-            e.target.style.top = (fireFoxEvt.pageY - teaEditor.offsetTop - Math.ceil(e.target.style.height.replace("px", "") / 2)) + teaEditor.scrollTop + "px";
-        } else {
-            e.target.style.left = (e.pageX - teaEditor.offsetLeft - Math.ceil(e.target.style.width.replace("px", "") / 2)) + teaEditor.scrollLeft + "px";
-            e.target.style.top = (e.pageY - teaEditor.offsetTop - Math.ceil(e.target.style.height.replace("px", "") / 2)) + teaEditor.scrollTop + "px";
-        }
+        moveElement(e);
     };
 
     function handleDragEnd(e) {
-        if (isFirefox) {
-            e.target.style.left = (fireFoxEvt.pageX - teaEditor.offsetLeft - Math.ceil(e.target.style.width.replace("px", "") / 2)) + teaEditor.scrollLeft + "px";
-            e.target.style.top = (fireFoxEvt.pageY - teaEditor.offsetTop - Math.ceil(e.target.style.height.replace("px", "") / 2)) + teaEditor.scrollTop + "px";
-        } else {
-            e.target.style.left = (e.pageX - teaEditor.offsetLeft - Math.ceil(e.target.style.width.replace("px", "") / 2)) + teaEditor.scrollLeft + "px";
-            e.target.style.top = (e.pageY - teaEditor.offsetTop - Math.ceil(e.target.style.height.replace("px", "") / 2)) + teaEditor.scrollTop + "px";
-        }
+        moveElement(e);
     };
 
     // Create new items.
     function connect(action, el) {
         let self = this;
         switch (action) {
+            // Tea
             case "start":
                 el.addEventListener('click', function(el) {
                     createStart();
@@ -446,6 +435,27 @@ window.Tea = tea = (function() {
                     renderLanguages();
                 });
                 break;
+                // ArchiMate
+            case "archimate-resource":
+                el.addEventListener('click', function(el) {
+                    createArchiMateResource();
+                });
+                break;
+            case "archimate-capability":
+                el.addEventListener('click', function(el) {
+                    createArchiMateCapability();
+                });
+                break;
+            case "archimate-value-stream":
+                el.addEventListener('click', function(el) {
+                    createArchiMateValueStream();
+                });
+                break;
+            case "archimate-course-of-action":
+                el.addEventListener('click', function(el) {
+                    createArchiMateCourseOfAction();
+                });
+                break;
         }
     }
 
@@ -470,6 +480,7 @@ window.Tea = tea = (function() {
         el.addEventListener('dragend', handleDragEnd, false);
     }
 
+    // Tea
     function createStart() {
         let el = document.createElement("div");
         el.className = "tea-component tea-start";
@@ -556,6 +567,138 @@ window.Tea = tea = (function() {
         teaArea.insertBefore(el, teaArea.childNodes[0]);
     }
 
+    // ArchiMate 3.1
+    function createArchiMateResource() {
+        let el = document.createElement("div");
+        el.className = "tea-component tea-archimate tea-archimate-resource";
+        el.style.left = (1 + teaEditor.scrollLeft) + "px";
+        el.style.top = (1 + teaEditor.scrollTop) + "px";
+        el.style.width = "119px";
+        el.style.height = "54px";
+        el.innerHTML = `<svg width="119" height="54" viewBox="0 0 119 54" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-miterlimit:10;">
+        <g transform="matrix(1,0,0,1,-1.69362,-1.60709)">
+            <g transform="matrix(1,0,0,1,-8.30638,-8.39291)">
+                <rect x="10" y="10" width="119" height="54" style="fill:rgb(245,222,170);"/>
+            </g>
+            <g transform="matrix(1,0,0,1,-8.30638,-8.39291)">
+                <clipPath id="_clip1">
+                    <rect x="9" y="9" width="122" height="57"/>
+                </clipPath>
+                <g clip-path="url(#_clip1)">
+                    <rect x="10" y="10" width="119" height="54" style="fill:none;stroke:rgb(20,14,0);stroke-width:1px;"/>
+                </g>
+            </g>
+            <g transform="matrix(1,0,0,1,-8.30638,-8.39291)">
+                <path d="M125,18.5C125,17.672 124.328,17 123.5,17L111.5,17C110.672,17 110,17.672 110,18.5L110,25.5C110,26.328 110.672,27 111.5,27L123.5,27C124.328,27 125,26.328 125,25.5L125,18.5Z" style="fill:none;stroke:black;stroke-width:1px;"/>
+            </g>
+            <g transform="matrix(1,0,0,1,-8.30638,-8.39291)">
+                <clipPath id="_clip2">
+                    <rect x="9" y="9" width="122" height="57"/>
+                </clipPath>
+                <g clip-path="url(#_clip2)">
+                    <path d="M127,20.5C127,20.224 126.776,20 126.5,20L125.5,20C125.224,20 125,20.224 125,20.5L125,23.5C125,23.776 125.224,24 125.5,24L126.5,24C126.776,24 127,23.776 127,23.5L127,20.5Z" style="fill:none;stroke:black;stroke-width:1px;"/>
+                </g>
+            </g>
+            <g transform="matrix(1,0,0,1,-8.30638,-8.39291)">
+                <path d="M113,19L113,25M116,19L116,25M119,19L119,25" style="fill:none;stroke:black;stroke-width:1px;"/>
+            </g>
+        </g>
+    </svg><input class="tea-archimate-innertext" contenteditable="true" value="resource">`;
+        el.setAttribute("draggable", "true");
+        attachAllDragEvents(el);
+        teaArea.insertBefore(el, teaArea.childNodes[0]);
+    }
+
+    function createArchiMateCapability() {
+        let el = document.createElement("div");
+        el.className = "tea-component tea-archimate tea-archimate-capability";
+        el.style.left = (1 + teaEditor.scrollLeft) + "px";
+        el.style.top = (1 + teaEditor.scrollTop) + "px";
+        el.style.width = "120px";
+        el.style.height = "55px";
+        el.innerHTML = `<svg width="120" height="55" viewBox="0 0 120 55" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-miterlimit:10;">
+        <g transform="matrix(1,0,0,1,-200.538,-9.5617)">
+            <path d="M320,20C320,14.481 315.519,10 310,10L211,10C205.481,10 201,14.481 201,20L201,54C201,59.519 205.481,64 211,64L310,64C315.519,64 320,59.519 320,54L320,20Z" style="fill:rgb(245,222,170);"/>
+        </g>
+        <g transform="matrix(1,0,0,1,-200.538,-9.5617)">
+            <g>
+                <path d="M320,20C320,14.481 315.519,10 310,10L211,10C205.481,10 201,14.481 201,20L201,54C201,59.519 205.481,64 211,64L310,64C315.519,64 320,59.519 320,54L320,20Z" style="fill:none;stroke:black;stroke-width:1px;"/>
+            </g>
+        </g>
+        <g>
+            <g transform="matrix(1,0,0,1,-200.538,-9.5617)">
+                <rect x="312" y="15" width="4" height="4" style="fill:none;stroke:black;stroke-width:1px;"/>
+            </g>
+            <g transform="matrix(1,0,0,1,-200.538,-9.5617)">
+                <rect x="308" y="19" width="4" height="4" style="fill:none;stroke:black;stroke-width:1px;"/>
+            </g>
+            <g transform="matrix(1,0,0,1,-200.538,-9.5617)">
+                <rect x="312" y="19" width="4" height="4" style="fill:none;stroke:black;stroke-width:1px;"/>
+            </g>
+            <g transform="matrix(1,0,0,1,-200.538,-9.5617)">
+                <rect x="304" y="23" width="4" height="4" style="fill:none;stroke:black;stroke-width:1px;"/>
+            </g>
+            <g transform="matrix(1,0,0,1,-200.538,-9.5617)">
+                <rect x="308" y="23" width="4" height="4" style="fill:none;stroke:black;stroke-width:1px;"/>
+            </g>
+            <g transform="matrix(1,0,0,1,-200.538,-9.5617)">
+                <rect x="312" y="23" width="4" height="4" style="fill:none;stroke:black;stroke-width:1px;"/>
+            </g>
+        </g>
+    </svg><input class="tea-archimate-innertext" contenteditable="true" value="capability">`;
+        el.setAttribute("draggable", "true");
+        attachAllDragEvents(el);
+        teaArea.insertBefore(el, teaArea.childNodes[0]);
+    }
+
+    function createArchiMateValueStream() {
+        let el = document.createElement("div");
+        el.className = "tea-component tea-archimate tea-archimate-value-stream";
+        el.style.left = (1 + teaEditor.scrollLeft) + "px";
+        el.style.top = (1 + teaEditor.scrollTop) + "px";
+        el.style.width = "120px";
+        el.style.height = "55px";
+        el.innerHTML = `<svg width="120" height="55" viewBox="0 0 120 55" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+        <g transform="matrix(1,0,0,1,0.5,0.5)">
+            <g transform="matrix(1,0,0,1,-393,-10)">
+                <path d="M512,20C512,14.481 507.519,10 502,10L403,10C397.481,10 393,14.481 393,20L393,54C393,59.519 397.481,64 403,64L502,64C507.519,64 512,59.519 512,54L512,20Z" style="fill:rgb(245,222,170);stroke:black;stroke-width:1px;"/>
+            </g>
+            <g transform="matrix(1,0,0,1,-394.138,-10.6114)">
+                <path d="M494,17L504,17L509,22L504,27L494,27L499,22L494,17Z" style="fill:none;fill-rule:nonzero;stroke:black;stroke-width:1px;stroke-linejoin:miter;stroke-miterlimit:10;"/>
+            </g>
+        </g>
+    </svg><input class="tea-archimate-innertext" contenteditable="true" value="value stream">`;
+        el.setAttribute("draggable", "true");
+        attachAllDragEvents(el);
+        teaArea.insertBefore(el, teaArea.childNodes[0]);
+    }
+
+    function createArchiMateCourseOfAction() {
+        let el = document.createElement("div");
+        el.className = "tea-component tea-archimate tea-archimate-course-of-action";
+        el.style.left = (1 + teaEditor.scrollLeft) + "px";
+        el.style.top = (1 + teaEditor.scrollTop) + "px";
+        el.style.width = "120px";
+        el.style.height = "55px";
+        el.innerHTML = `<svg width="120" height="55" viewBox="0 0 120 55" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+        <g transform="matrix(1,0,0,1,-9.5,-93.6827)">
+            <path d="M129,104C129,98.481 124.519,94 119,94L20,94C14.481,94 10,98.481 10,104L10,138C10,143.519 14.481,148 20,148L119,148C124.519,148 129,143.519 129,138L129,104Z" style="fill:rgb(245,222,170);stroke:black;stroke-width:1px;"/>
+        </g>
+        <g transform="matrix(1,0,0,1,-9.5,-93.6827)">
+            <path d="M107.6,106L113.6,107L110.6,112.2"/>
+        </g>
+        <g transform="matrix(1,0,0,1,-9.5,-93.6827)">
+            <path d="M110.5,109C108.074,109 105.997,110.742 105.576,113.132" style="fill:none;stroke:black;stroke-width:2px;stroke-linejoin:miter;stroke-miterlimit:10;"/>
+        </g>
+        <g transform="matrix(1,0,0,1,-9.5,-93.6827)">
+            <path d="M126,103.5C126,99.91 123.09,97 119.5,97C115.91,97 113,99.91 113,103.5C113,107.09 115.91,110 119.5,110C123.09,110 126,107.09 126,103.5ZM123.5,103.5C123.5,101.291 121.709,99.5 119.5,99.5C117.291,99.5 115.5,101.291 115.5,103.5C115.5,105.709 117.291,107.5 119.5,107.5C121.709,107.5 123.5,105.709 123.5,103.5ZM121,103.5C121,102.672 120.328,102 119.5,102C118.672,102 118,102.672 118,103.5C118,104.328 118.672,105 119.5,105C120.328,105 121,104.328 121,103.5ZM120,103.5C120,103.224 119.776,103 119.5,103C119.224,103 119,103.224 119,103.5C119,103.776 119.224,104 119.5,104C119.776,104 120,103.776 120,103.5Z" style="fill:none;stroke:black;stroke-width:1.2px;stroke-linejoin:miter;stroke-miterlimit:10;"/>
+        </g>
+    </svg><input class="tea-archimate-innertext" contenteditable="true" value="course of action">`;
+        el.setAttribute("draggable", "true");
+        attachAllDragEvents(el);
+        teaArea.insertBefore(el, teaArea.childNodes[0]);
+    }
+    // Init
     function init(cssSelector) {
         // Set css selector.
         cssSelector = cssSelector;
@@ -580,9 +723,18 @@ window.Tea = tea = (function() {
 
 document.addEventListener('DOMContentLoaded', (event) => {
     Tea.init(".tea-editor");
+
+    // Tea
     Tea.connect('start', document.getElementById('tea-start'));
     Tea.connect('stop', document.getElementById('tea-stop'));
     Tea.connect('action', document.getElementById('tea-action'));
     Tea.connect('variable', document.getElementById('tea-variable'));
     Tea.connect('import', document.getElementById('tea-import'));
+
+    // ArchiMate 3.1
+    Tea.connect('archimate-resource', document.getElementById('tea-archimate-resource'));
+    Tea.connect('archimate-capability', document.getElementById('tea-archimate-capability'));
+    Tea.connect('archimate-value-stream', document.getElementById('tea-archimate-value-stream'));
+    Tea.connect('archimate-course-of-action', document.getElementById('tea-archimate-course-of-action'));
+
 });
